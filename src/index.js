0bd4f601c4e1fce146e5f5ba2df97252a6efc160
FelicityNyoni <felinyoni29@gmail.com>
2024-11-27 21:22:19 +0200

    Fixed a class that was not working

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${
 
 let poemElement = document.querySelector("#poem");
 poemElement.classList.remove("hidden");
-poemElement.innerHTML = `<div class="generating">⌛Generating a poem about${userInstructionsElement.value}</div>`;
+poemElement.innerHTML = `<div class="generating">⌛Generating a poem about ${userInstructionsElement.value}</div>`;
 axios.get(apiUrl).then(displayPoem);
 }
 
